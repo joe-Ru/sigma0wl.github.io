4f2e433ab650c7be4ba501e5f746b6503c2f8af8
sigma0wl <ruizjoseph17@gmail.com>
2020-01-02 23:01:00 -0500

    initial commit
    Contains all updated files from the SWNWebsite repo

---
+++ src/pages/CombatNPC.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+
+
+class CombatNPC extends React.Component {
+    render() {
+       return <div>
+            <Layout>
+
+            </Layout>
+        </div>
+    }
+}
+
+export default CombatNPC
---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => <Layout>
+               <h1>Table Top Dice Roller Website</h1>
+                <p>Welcome fellow Space Traveler! </p>
+            </Layout>
+
