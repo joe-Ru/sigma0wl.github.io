eb4be0eaf16dd8309a6a9b7e5a495c14d3899ee5
sigma0wl <ruizjoseph17@gmail.com>
2020-01-08 10:22:27 -0500

    packages idk
    yeeting packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,8 @@ module.exports = {
           background_color: `#f7f0eb`,
           theme_color: `#a2466c`,
           display: `standalone`,
-          icon: `src/images/dice-icon.png`
+          icon: `src/images/dice-icon.png`,
+          pathPrefix: "/sigma0wl.github.io"
         },
       },
     ],
