f9be18ba0100df787d16c9e35ee4aea3721b3c95
sigma0wl <ruizjoseph17@gmail.com>
2020-01-11 12:41:17 -0500

    Updates

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () => <Layout>
-               <h1>Table Top Dice Roller Website</h1>
-                <p>Welcome fellow Space Traveler! </p>
-            </Layout>
-
---
+++ styles-786cdce2a3e2a695abb1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{I696:function(n,w,o){}}]);
+//# sourceMappingURL=styles-786cdce2a3e2a695abb1.js.map
